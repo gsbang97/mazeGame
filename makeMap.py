07e8719b97d8fcda3a86b84a13ac2d598b2516bf
gsbang97 <gsbang@inha.edu>
2022-10-30 13:53:28 +0900

    BFS로 목적지 가능한 지점만 넣기

--- makeMap.py
+++ makeMap.py
@@ -1,4 +1,12 @@
+from copy import deepcopy
 import random
+visited = []
+bfs_map = []
+def BFS(maze_map,x,y):
+    global visited,bfs_map
+
+    
+
 def makeMap()->list:
     maze_map= [
         [0,0,0,0,0,0,0,0,0,0],
@@ -18,15 +26,60 @@ def makeMap()->list:
     random.shuffle(wall_list)
     for i in wall_list[:wall_num]:
         maze_map[i//10][i%10] = 1
-    while True:
-        x = random.randint(0,9)
-        y = random.randint(0,9)
-        if(maze_map[x][y]==0):
-            maze_map[x][y] = 2
-            break
+    
+    global visited,bfs_map
+    for x in maze_map:
+        print(x)
+    print()
+    bfs_map = deepcopy(maze_map)
+    bfs_map[0][0] = 1
+    visited = [(0,0)]
+    que = [(0,0)]
+    depth = 1
+    while que:
+        x,y = que.pop(0)
+        depth =bfs_map[x][y] + 1
+        if x>0 and (x-1,y) not in visited and maze_map[x-1][y] != 1:
+            que.append((x-1,y))
+            visited.append((x-1,y))
+            bfs_map[x-1][y] = depth
+        if y>0 and (x,y-1) not in visited and maze_map[x][y-1] != 1:
+            que.append((x,y-1))
+            visited.append((x,y-1))
+            bfs_map[x][y-1] = depth
+        if x<9 and (x+1,y) not in visited and maze_map[x+1][y] != 1:
+            que.append((x+1,y))
+            visited.append((x+1,y))
+            bfs_map[x+1][y] = depth
+        if y<9 and (x,y+1) not in visited and maze_map[x][y+1] != 1:
+            que.append((x,y+1))
+            visited.append((x,y+1))
+            bfs_map[x][y+1] = depth
+        #print('x',x,'y',y,'que:',que,'d',depth)
+    m = depth-1 if depth < 11 else 10
+    
+    goal_list = []
+    for i in range(10):
+        for j in range(10):
+            if bfs_map[i][j] >= m:
+                goal_list.append((i,j))
+    #print(m,goal_list)
+    x,y = random.choice(goal_list)
+    
+    maze_map[x][y] = 2
+    #print(bfs_map)
+
+    # while True:
+    #     x = random.randint(0,9)
+    #     y = random.randint(0,9)
+    #     if(maze_map[x][y]==0):
+    #         maze_map[x][y] = 2
+    #         break
     for x in range(10) :
         maze_map[x] = [1]+maze_map[x]+[1]
     
     maze_map = [[1 for _ in range(12)]] + maze_map + [[1 for _ in range(12)]]
     
     return maze_map
+# for x in makeMap():
+#     print(x)
